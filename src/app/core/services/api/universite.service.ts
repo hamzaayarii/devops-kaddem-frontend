e28ef273f949e084d659c2809f84a45553a89708
Hamza Ayari <123635389+hamzaayarii@users.noreply.github.com>
2025-04-14 03:38:06 +0100

    Update universite.service.ts

--- src/app/core/services/api/universite.service.ts
+++ src/app/core/services/api/universite.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Universite } from 'src/app/shared/models/universite.model';
-
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
